c7d42bac2d9070dacb7e8d411a4e404063b649a7
Agustin Lopez <agustinlpzb@gmail.com>
2023-10-01 17:03:05 +0200

    Update script.js
    Removed a part of the script.

--- script.js
+++ script.js
@@ -7,29 +7,6 @@ function computerPlay() {
 // let randomString = computerPlay();
 // console.log(randomString);
 
-function gamePlay(playerSelection, computerSelection) {
-  const formattedPlayerSelection = playerSelection.toLowerCase();
-  const formattedComputerSelection = computerSelection.toLowerCase();
-
-  if (formattedPlayerSelection === formattedComputerSelection) {
-    return "Its a tie!";
-  }
-  if (
-    (formattedPlayerSelection === "rock" &&
-      formattedComputerSelection === "scissors") ||
-    (formattedPlayerSelection === "scissors" &&
-      formattedComputerSelection === "paper") ||
-    (formattedPlayerSelection === "paper" &&
-      formattedComputerSelection === "rock")
-  ) {
-    return `You win, your ${formattedPlayerSelection} beats his ${formattedComputerSelection}`;
-  } else {
-    return `You loose your ${formattedComputerSelection} beats his ${formattedPlayerSelection}`;
-  }
-}
-
-console.log(gamePlay(computerPlay(), computerPlay()));
-
 function playRound() {
   let playerSelection = prompt("Write rock, paper or scissors");
   playerSelection = playerSelection.toLowerCase().trim().replace(/\s/g, "");
