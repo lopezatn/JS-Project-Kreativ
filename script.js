3060bdf1ea8fd2c587a8c5a3be4ed8ca0a92de6b
DomantasMar <addyman777@gmail.com>
2023-10-01 16:27:50 +0300

    added index.html and playRound function

--- script.js
+++ script.js
@@ -1,27 +1,63 @@
 function computerPlay() {
-    const rps = ["rock", "paper", "scissors"];
-    var randomIndex = Math.floor(Math.random() * rps.length);
-    return rps[randomIndex];
+  const rps = ["rock", "paper", "scissors"];
+  var randomIndex = Math.floor(Math.random() * rps.length);
+  return rps[randomIndex];
 }
 
 // let randomString = computerPlay();
 // console.log(randomString);
 
 function gamePlay(playerSelection, computerSelection) {
-    const formattedPlayerSelection = playerSelection.toLowerCase()
-    const formattedComputerSelection = computerSelection.toLowerCase();
+  const formattedPlayerSelection = playerSelection.toLowerCase();
+  const formattedComputerSelection = computerSelection.toLowerCase();
 
-    if (formattedPlayerSelection === formattedComputerSelection) {
-        return "Its a tie!";
-    }
-    if ((formattedPlayerSelection === "rock" && formattedComputerSelection === "scissors") ||
-        (formattedPlayerSelection === "scissors" && formattedComputerSelection === "paper") ||
-        (formattedPlayerSelection === "paper" && formattedComputerSelection === "rock")) {
-        return `You win, your ${formattedPlayerSelection} beats his ${formattedComputerSelection}`
-    } else {
-        return `You loose your ${formattedComputerSelection} beats his ${formattedPlayerSelection}`
-    }
-    
+  if (formattedPlayerSelection === formattedComputerSelection) {
+    return "Its a tie!";
+  }
+  if (
+    (formattedPlayerSelection === "rock" &&
+      formattedComputerSelection === "scissors") ||
+    (formattedPlayerSelection === "scissors" &&
+      formattedComputerSelection === "paper") ||
+    (formattedPlayerSelection === "paper" &&
+      formattedComputerSelection === "rock")
+  ) {
+    return `You win, your ${formattedPlayerSelection} beats his ${formattedComputerSelection}`;
+  } else {
+    return `You loose your ${formattedComputerSelection} beats his ${formattedPlayerSelection}`;
+  }
 }
 
-console.log(gamePlay(computerPlay(), computerPlay()))
+console.log(gamePlay(computerPlay(), computerPlay()));
+
+function playRound() {
+  let playerSelection = prompt("Write rock, paper or scissors");
+  playerSelection = playerSelection.toLowerCase().trim().replace(/\s/g, "");
+  const computerSelection = computerPlay();
+  let winner;
+  //   console.log(`Player input: ${playerSelection}`);
+  //   console.log(`Computer input: ${computerSelection}`);
+  switch (playerSelection) {
+    case "rock":
+      if (computerSelection === "paper") return (winner = "computer");
+      if (computerSelection === "rock") return (winner = "tie");
+      if (computerSelection === "scissors") return (winner = "player");
+      break;
+    case "paper":
+      if (computerSelection === "scissors") return (winner = "computer");
+      if (computerSelection === "paper") return (winner = "tie");
+      if (computerSelection === "rock") return (winner = "player");
+      break;
+    case "scissors":
+      if (computerSelection === "rock") return (winner = "computer");
+      if (computerSelection === "scissors") return (winner = "tie");
+      if (computerSelection === "paper") return (winner = "player");
+      break;
+    default:
+      alert("Invalid user input");
+      playRound();
+      break;
+  }
+}
+
+console.log(playRound());
