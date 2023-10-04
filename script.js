5c34945d13501899b6f0f888b13e4763a0ab581d
k1shor <k1shor@hotmail.com>
2023-10-04 12:30:13 +0545

    changes by kishor

--- script.js
+++ script.js
@@ -4,13 +4,8 @@ function computerPlay() {
   return rps[randomIndex];
 }
 
-// let randomString = computerPlay();
-// console.log(randomString);
-
 function playRound(playerSelection, computerSelection) {
   let winner;
-  //   console.log(`Player input: ${playerSelection}`);
-  //   console.log(`Computer input: ${computerSelection}`);
   switch (playerSelection) {
     case "rock":
       if (computerSelection === "paper") return (winner = "computer");
@@ -29,13 +24,49 @@ function playRound(playerSelection, computerSelection) {
       break;
     default:
       alert("Invalid user input");
-      playRound();
       break;
   }
 }
 
 
-let playerSelection = prompt("Write rock, paper or scissors");
-playerSelection = playerSelection.toLowerCase().replace(/\s/g, "");
-let computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  let playerWins = 0;
+  let computerWins = 0;
+
+  for (let i = 1; i <= 5; i++) {
+    let playerSelection = prompt("Choose rock, paper or scissors");
+    playerSelection = playerSelection.toLowerCase().replace(/\s/g, "");
+    let computerSelection = computerPlay();
+    let winner = playRound(playerSelection, computerSelection);
+
+    switch (winner) {
+      case "player":
+        playerWins++;
+        console.log(`Woohoo, You won this round!`);
+        break;
+      case "computer":
+        computerWins++;
+        console.log("Computer wins this round");
+        break;
+      case "tie":
+        console.log("It's a tie. Lets try one more time.");
+        i--;
+        break;
+      default:
+        console.log("Wrong Input")
+        i--;
+    }
+
+    console.log(`Score --------------------- Player: ${playerWins} ---- Computer: ${computerWins}`);
+    i>0 && console.log(`${5 - i} rounds left`);
+  }
+  if (computerWins < playerWins) {
+    console.log("Congratulations, You have defeated me!!! I will have my revenge soon.");
+  }
+  else {
+    console.log("Huhuhahahahha, You can not defeat me. !!! Better Luck next time.");
+  }
+}
+
+// console.log(playRound());
+game();
