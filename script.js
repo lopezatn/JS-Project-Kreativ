e6d723991b2ad6e0c7d250e5c73863a89b6f7182
DomantasMar <addyman777@gmail.com>
2023-10-08 19:56:16 +0300

    minor fixes and delay script loading

--- script.js
+++ script.js
@@ -1,88 +1,101 @@
-function computerPlay() {
-  const rps = ["rock", "paper", "scissors"];
-  let randomIndex = Math.floor(Math.random() * rps.length);
-  return rps[randomIndex];
-}
-
-function playRound(playerSelection, computerSelection) {
-  let winner;
-  switch (playerSelection) {
-    case "rock":
-      if (computerSelection === "paper") return (winner = "computer");
-      if (computerSelection === "rock") return (winner = "tie");
-      if (computerSelection === "scissors") return (winner = "player");
-      break;
-    case "paper":
-      if (computerSelection === "scissors") return (winner = "computer");
-      if (computerSelection === "paper") return (winner = "tie");
-      if (computerSelection === "rock") return (winner = "player");
-      break;
-    case "scissors":
-      if (computerSelection === "rock") return (winner = "computer");
-      if (computerSelection === "scissors") return (winner = "tie");
-      if (computerSelection === "paper") return (winner = "player");
-      break;
-    case null:
-      return "cancelled"
-    default:
-      break;
+window.onload = function () {
+  function computerPlay() {
+    const rps = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random() * rps.length);
+    return rps[randomIndex];
   }
-}
 
-function playerPlay() {
-  return prompt(
-    `Write rock, paper or scissors, all results provided in console, to open "CTRL + Shift + i`
-  );
-}
-
-function game() {
-  let playerWins = 0;
-  let computerWins = 0;
-  let escape = false;
-
-  for (let i = 1; i <= 5 && escape === false; i++) {
-    let playerSelection = playerPlay()
-    if (playerSelection != null)
-      playerSelection = playerSelection.toLowerCase().replace(/\s/g, "");
-
-    let computerSelection = computerPlay();
-    let winner = playRound(playerSelection, computerSelection);
-
-    switch (winner) {
-      case "player":
-        playerWins++;
-        console.log(`Woohoo, You won this round!`);
+  function playRound(playerSelection, computerSelection) {
+    let winner;
+    switch (playerSelection) {
+      case "rock":
+        if (computerSelection === "paper") return (winner = "computer");
+        if (computerSelection === "rock") return (winner = "tie");
+        if (computerSelection === "scissors") return (winner = "player");
         break;
-      case "computer":
-        computerWins++;
-        console.log("Computer wins this round");
+      case "paper":
+        if (computerSelection === "scissors") return (winner = "computer");
+        if (computerSelection === "paper") return (winner = "tie");
+        if (computerSelection === "rock") return (winner = "player");
         break;
-      case "tie":
-        console.log("It's a tie. Lets try one more time.");
-        i--;
+      case "scissors":
+        if (computerSelection === "rock") return (winner = "computer");
+        if (computerSelection === "scissors") return (winner = "tie");
+        if (computerSelection === "paper") return (winner = "player");
         break;
-      case "cancelled":
-        console.log("Exiting game..");
-        escape = true;
+      case null:
+        return "cancelled";
       default:
-        console.log("Wrong Input");
-        i--;
+        break;
     }
-
-    console.log(
-      `Score --------------------- Player: ${playerWins} ---- Computer: ${computerWins}`
-    );
-    i > 0 && console.log(`${5 - i} rounds left`);
   }
-  if (computerWins < playerWins) {
-    console.log(
-      "Congratulations, You have defeated me!!! I will have my revenge soon."
-    );
-  } else {
-    console.log(
-      "Huhuhahahahha, You can not defeat me. !!! Better Luck next time."
-    );
-  }
-}
 
-// game();
+  function playerPlay() {
+    let playerInput = prompt(
+      `Write rock, paper or scissors, all results provided in console, to open "CTRL + Shift + i"`
+    );
+    if (playerInput !== null) {
+      playerInput = playerInput.toLowerCase().replace(/\s/g, "");
+      return playerInput;
+    } else {
+      alert("See you next time, when you're ready to lose :)");
+    }
+  }
+
+  function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    let ties = 0;
+
+    for (let i = 1; i <= 5; i++) {
+      let playerSelection = playerPlay();
+
+      let computerSelection = computerPlay();
+      let winner = playRound(playerSelection, computerSelection);
+
+      switch (winner) {
+        case "player":
+          playerWins++;
+          console.log(`Woohoo, You won this round!`);
+          console.log(`Player chose: ${playerSelection}`);
+          console.log(`Computer chose: ${computerSelection}`);
+          break;
+        case "computer":
+          computerWins++;
+          console.log(`Computer wins this round!`);
+          console.log(`Player chose: ${playerSelection}`);
+          console.log(`Computer chose: ${computerSelection}`);
+          break;
+        case "tie":
+          console.log(`It's a tie. No one scores a point!`);
+          console.log(`Player chose: ${playerSelection}`);
+          console.log(`Computer chose: ${computerSelection}`);
+          ties++;
+          break;
+        case null:
+          return (i = 5);
+        default:
+          console.log("Wrong Input");
+          i--;
+      }
+
+      console.log(
+        `Score --------------------- Player: ${playerWins} ---- Computer: ${computerWins}`
+      );
+      i > 0 && console.log(`${5 - i} rounds left`);
+    }
+    if (computerWins < playerWins) {
+      console.log(
+        "Congratulations, You have defeated me!!! I will have my revenge soon."
+      );
+    } else if (computerWins === playerWins) {
+      console.log("Well it's a tie, atleast you didn't lose");
+    } else {
+      console.log(
+        "Huhuhahahahha, You can not defeat me. !!! Better Luck next time."
+      );
+    }
+  }
+
+  game();
+};
